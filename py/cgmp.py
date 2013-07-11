908b643b19fb1cdccff955ca6f80453b55183736
Dmitry Ponyatov <dponyatov@gmail.com>
2013-07-11 15:59:14 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py/cgmp.py
+++ py/cgmp.py
@@ -2,15 +2,25 @@
 
 # CityGuide/MP file formats
 
-import os,time
+import os,time,re
 yy,mm,dd,hh=time.localtime()[:4]
 NOW='%.2i%.2i%.2i%.2i'%(yy%100,mm,dd,hh)
 
+TYPE={
+      'MainCity':0x0100,
+      'Coverage':0x004B
+}
+
+def nosp(str):
+    return re.sub(r'\s+',r'',str)
+
 class IMGID:
-    Id=NOW
+    ID=NOW
     Name=''
     Version=''
     Copyright=''
+    MainTown=''
+    PointView='0 0'
     def __init__(self):
         pass
     def __str__(self):
@@ -22,6 +32,8 @@ CodePage=65001
 ID=%s
 Name=%s
 LocalName=%s
+MainTown=%s
+PointView=%s
 Version=%s
 VersionSub=%s
 Copyright=%s
@@ -30,52 +42,61 @@ Elevation=m
 Levels=1
 Level0=26
 [END]
-'''%(self.Id,self.Name,self.Name,VV,VS,self.Copyright)
+'''%(self.ID,self.Name,self.Name,self.MainTown,self.PointView,VV,VS,self.Copyright)
 
 class POI:
-    def __init__(self,type,lat,lon,label=''):
+    def __init__(self,id,type,lat,lon,label=''):
+        self.id=id
         self.type=type
         self.lat=lat
         self.lon=lon
         self.label=label
     def __str__(self):
         return '''
-[POI]
+[POI] ; id:%s
 Data0=(%s,%s)
-Type=0x%.4X
+Type=0x%x
 Label=%s
 [END]
-'''%(self.lat,self.lon,self.type,self.label)
+'''%(self.id,self.lat,self.lon,self.type,self.label)
+
+class PLINE:
+    def __init__(self,id,type,coords,label=''):
+        self.id=id
+        self.type=type
+        self.coords=coords
+        self.label=label
+    def __str__(self):
+        return '''
+; id:%s
+[POLYGON] 
+Data0=%s
+Type=0x%x
+Label=%s
+[END]
+'''%(self.id,nosp(str(self.coords)[1:-1]),self.type,self.label)
 
 class MP:
+#     lats=[]
+#     lons=[]
     def __init__(self,FN):
         self.FileName=FN
         self.IMGID=IMGID()
-        self.POI=[POI(0x0100,0,0,'Main City')]
+        self.POI=[]
+        self.PLINE=[]
+    def addPOI(self,id,type,lat,lon,label=''):
+        self.POI+=[POI(id,type,lat,lon,label)]
+    def addPLINE(self,id,type,coords,label=''):
+        self.PLINE+=[PLINE(id,type,coords,label)]
     def dump(self):
-        F=open(self.FileName,'w') ; F.write(str(self)); F.close()
+        F=open(self.FileName,'w') ; 
+        F.write(str(self)); 
+        F.close()
     def __str__(self):
         S=str(self.IMGID)
         for i in self.POI:
             S+=str(i)
+        for i in self.PLINE[:1]:
+            S+=str(i)
         return S
-
-class POLY:
-    def __init__(self,type,points,label=[]):
-        self.points=points
-        self.type=type
-        self.label=label
-    def pntstr(self):
-        T=''
-        for i in self.points:
-            T+='%s,'%str(i)
-        if T:
-            return T[:-1]
-        else:
-            return ''
-    def __str__(self):
-        return '[POLY]\nData0=%s\nType=0x%.4X\nLabel=%s\n[END]\n'%(self.pntstr(),self.type,self.label)
-
-Map_Coverage_Area = POLY(0x004b,[(-1,-1),(+1,-1),(+1,+1),(-1,+1)],'Map Coverage')
-
-
+    
--- py/py.py
+++ py/py.py
@@ -4,7 +4,7 @@ import os,time
 yy,mm,dd,hh=time.localtime()[:4]
 NOW='%.2i%.2i%.2i%.2i'%(yy%100,mm,dd,hh)
  
-import cfg,osm,cgmp
+import cfg,osm,cgmp,lxml
 
 SAMARA_OBL = osm.relation(id=72194)
 OSM        = SAMARA_OBL.xml.xpath('//osm')[0]
@@ -18,27 +18,29 @@ MP.IMGID.Copyright=OSM.attrib['copyright']
 NODES={}
 for i in SAMARA_OBL.xml.xpath('//node'):
     T=i.attrib
-    UID=T['uid'] ; LAT=T['lat'] ; LON=T['lon']
-    NODES[UID]=(LAT,LON) #; print UID,NODES[UID]
+    ID=T['id'] ; LAT=float(T['lat']) ; LON=float(T['lon'])
+    MP.addPOI(ID,0,LAT,LON,'')
+    NODES[ID]=(LAT,LON) #; print UID,NODES[UID]
     
-print SAMARA_OBL.xml.xpath("//*['capital'='yes']")
+WAYS={}
+for i in SAMARA_OBL.xml.xpath("//way[@visible='true']"):
+    T=i.attrib
+    ID=T['id']
+    ND=[]
+    for j in i.xpath("//nd"):
+        ND+=[NODES[j.attrib['ref']]]
+    MP.addPLINE(ID,0x22,ND,'')
 
-# for i in NODES:
-#     MP.addPOI()
-#     print i,NODES[i]
-    
+RegionCapital = SAMARA_OBL.xml.xpath("/osm/node[tag[@k='capital']]")[0]    
+id,lat,lon = RegionCapital.attrib['id'],RegionCapital.attrib['lat'],RegionCapital.attrib['lon']
+name = RegionCapital.xpath("tag[@k='name:ru']")[0].attrib['v']
+MP.addPOI(id,cgmp.TYPE['MainCity'],lat,lon,name)
+MP.IMGID.MainTown=name
+
+RegionCenter = SAMARA_OBL.xml.xpath("/osm/node[tag[@k='place'][@v='state']]")[0]
+lat,lon = RegionCenter.attrib['lat'],RegionCenter.attrib['lon']
+MP.IMGID.PointView='%s %s'%(lat,lon)
+
 MP.dump()
                      
-# VV,SV=R.attrib['version'].split('.')
-# CC='(c) '+R.attrib['copyright'].split()[0]+' (gpl) dponyatov@gmail.com'
-# print >>MP,cgmp.IMGID(Version=VV,SubVersion=SV,Copyright=CC)
-# 
-# print >>MP,cgmp.Map_Coverage_Area
-# print >>MP,cgmp.Main_City
-# 
-# for i in X.findall('node'):
-#     T=i.attrib
-#     print >>MP,cgmp.POI(0,(T['lat'],T['lon']),T['uid'])
-#     #'; %s\n[POI]\nData0=(%s,%s)\nLabel=%s\n[END]\n'%(,,T['uid'])
-
 os.system(r'..\bin\MP2CGMap.exe mp:..\map\mp.mp scale:10000')
